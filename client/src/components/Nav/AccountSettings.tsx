f12dda57fc60ddc7107afb2c38cfb06d1592e360
FaridBustos <faridbustoc@gmail.com>
2026-04-02 09:50:30 -0500

    refactor(AccountSettings) use email instead of username

--- client/src/components/Nav/AccountSettings.tsx
+++ client/src/components/Nav/AccountSettings.tsx
@@ -36,7 +36,7 @@ function AccountSettings() {
           className="mt-2 grow overflow-hidden text-ellipsis whitespace-nowrap text-left text-text-primary"
           style={{ marginTop: '0', marginLeft: '0' }}
         >
-          {user?.name ?? user?.username ?? localize('com_nav_user')}
+          {user?.email ?? localize('com_nav_user')}
         </div>
       </Select.Select>
       <Select.SelectPopover
